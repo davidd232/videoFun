f97ff0668db9c67d3ab20887f69f1e52d014447e
David Darley <daviddar232@gmail.com>
2018-03-01 16:37:54 -0800

    [add] - adding a color picker from react-color and properly handling color changes.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { SketchPicker } from 'react-color';
 
 class App extends Component {
   constructor() {
@@ -10,7 +11,7 @@ class App extends Component {
       lastY: 0,
       canvas: '',
       ctx: '',
-      hue: `hsl(85%, 100%, 50%)`
+      hue: '#095'
     }
   }
   componentDidMount() {
@@ -18,7 +19,7 @@ class App extends Component {
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    ctx.strokeStyle = '#0af';
+    // ctx.strokeStyle = '#0af';
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
     ctx.lineWidth = 5;
@@ -27,6 +28,16 @@ class App extends Component {
       canvas,
       ctx
     })
+    var webrtc = new SimpleWebRTC({
+      // the id/element dom element that will hold "our" video
+      localVideoEl: 'localVideo',
+      // immediately ask for camera access
+      autoRequestMedia: true
+    });
+    webrtc.on('readyToCall', function () {
+      // you can name it anything
+      webrtc.joinRoom('your awesome room name');
+    });
   }
   mouseMove(e) {
     this.draw(e);
@@ -60,27 +71,49 @@ class App extends Component {
     this.state.ctx.beginPath();
     this.state.ctx.moveTo(this.state.lastX, this.state.lastY);
     this.state.ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
-    this.state.ctx.strokeStyle = '#000';
+    this.state.ctx.strokeStyle = this.state.hue;
     this.state.ctx.stroke();
     this.setState({
       lastX: e.nativeEvent.offsetX,
       lastY: e.nativeEvent.offsetY
     })
   }
-
+  handleClick() {
+    console.log(this.state);
+    this.state.ctx.save();
+    this.state.ctx.setTransform(1, 0, 0, 1, 0, 0);
+    // Will always clear the right space
+    this.state.ctx.clearRect(0, 0, this.state.canvas.width, this.state.canvas.height);
+    this.state.ctx.restore()
+  }
+  handleChangeComplete(color) {
+    this.setState({
+      hue: color.hex
+    });
+  };
   render() {
     return (
-      <div>
-        {/* <video></video> */}
-        <canvas
-          onMouseMove={this.mouseMove.bind(this)}
-          onMouseOut={this.mouseOut.bind(this)}
-          onMouseDown={this.mouseDown.bind(this)}
-          onMouseUp={this.mouseUp.bind(this)}
-          ref="myCanvas"
-          id="draw"
-        >
-        </canvas>
+      <div >
+        <div>
+          <button className='clearBtn' onClick={this.handleClick.bind(this)}>Clear Drawing</button>
+          <SketchPicker 
+            color={this.state.hue}
+            onChangeComplete={this.handleChangeComplete.bind(this)}
+          />
+        </div>
+        <div className='container'>
+          <video className='video' id='localVideo'></video>
+          <canvas
+            className='canvas'
+            onMouseMove={this.mouseMove.bind(this)}
+            onMouseOut={this.mouseOut.bind(this)}
+            onMouseDown={this.mouseDown.bind(this)}
+            onMouseUp={this.mouseUp.bind(this)}
+            ref="myCanvas"
+            id="draw"
+          >
+          </canvas>
+        </div>
       </div>
     );
   }
